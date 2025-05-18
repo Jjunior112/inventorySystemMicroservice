d8970ae384c11acc33d7cc6af9c8813d63dde064
jjunior112 <juarezgcjr@gmail.com>
2025-05-18 20:35:10 -0300

    ci: ajustes no dockerfile e docker compose

---
+++ Frontend/inventorySystem/src/Components/NavBar.tsx
@@ -0,0 +1,17 @@
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="">Pagina inicial</a></li>
+                <li><a href="">Produtos</a></li>
+                <li><a href="">Operações</a></li>
+              
+            </ul>
+
+        </nav>
+    )
+}
+
+export default NavBar
